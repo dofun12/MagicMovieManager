6cdb9f1dd72d9765fbf17539aaf081a772f44aa0
LemanoMan <k.v.n.such@gmail.com>
2015-11-14 13:03:35 -0200

    asda

--- src/main/java/org/lemanoman/testeweb/model/HistoricoModel.java
+++ src/main/java/org/lemanoman/testeweb/model/HistoricoModel.java
@@ -2,28 +2,74 @@ package org.lemanoman.testeweb.model;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="historico")
 public class HistoricoModel {
-	private Integer idSerieFile;
-	private Date lastWatched;
-	private Long lastPosition;
+    	@Id
+    	@GeneratedValue
+    	private Integer codigo;
+
+    	@Column
+    	private Integer idSerieFile;
+    	
+    	@Transient
+	private MPHCResponseModel status;
+    	
+    	@Column
+	private Date lastTimeWatched;
+    	
+    	@Column
+	private Double percentWatched;
+
+	public Integer getCodigo() {
+	    return codigo;
+	}
+
+	public void setCodigo(Integer codigo) {
+	    this.codigo = codigo;
+	}
+
 	public Integer getIdSerieFile() {
-		return idSerieFile;
+	    return idSerieFile;
 	}
+
 	public void setIdSerieFile(Integer idSerieFile) {
-		this.idSerieFile = idSerieFile;
+	    this.idSerieFile = idSerieFile;
 	}
-	public Date getLastWatched() {
-		return lastWatched;
+
+	public MPHCResponseModel getStatus() {
+	    return status;
 	}
-	public void setLastWatched(Date lastWatched) {
-		this.lastWatched = lastWatched;
+
+	public void setStatus(MPHCResponseModel status) {
+	    this.status = status;
 	}
-	public Long getLastPosition() {
-		return lastPosition;
+
+	public Date getLastTimeWatched() {
+	    return lastTimeWatched;
 	}
-	public void setLastPosition(Long lastPosition) {
-		this.lastPosition = lastPosition;
+
+	public void setLastTimeWatched(Date lastTimeWatched) {
+	    this.lastTimeWatched = lastTimeWatched;
 	}
-	
-	
+
+	public Double getPercentWatched() {
+	    return percentWatched;
+	}
+
+	public void setPercentWatched(Double percentWatched) {
+	    this.percentWatched = percentWatched;
+	}
 }
