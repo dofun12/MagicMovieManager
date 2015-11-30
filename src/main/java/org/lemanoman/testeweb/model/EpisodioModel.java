482d2de293172791aac9c52bc90dea7237560921
kevim <k.v.n.such@gmail.com>
2015-11-30 18:25:39 -0200

    Bugs To meeeeeeeee

--- src/main/java/org/lemanoman/testeweb/model/EpisodioModel.java
+++ src/main/java/org/lemanoman/testeweb/model/EpisodioModel.java
@@ -1,27 +1,29 @@
 package org.lemanoman.testeweb.model;
 
+import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "episodio")
 public class EpisodioModel {
-    @EmbeddedId
-    private EpisodioPK pk;
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    @Column(name="id_serie",nullable=false)
+    private Integer idSerie;
 
+    @Column
+    private String name;
+    
     @Column(name="id_mediafile",nullable=false)
     private Integer idMediafile;
     
-    public EpisodioPK getPk() {
-        return pk;
-    }
-
-    public void setPk(EpisodioPK pk) {
-        this.pk = pk;
-    }
-
     public Integer getIdMediafile() {
         return idMediafile;
     }
@@ -30,12 +32,20 @@ public class EpisodioModel {
         this.idMediafile = idMediafile;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     @Override
     public int hashCode() {
 	final int prime = 31;
 	int result = 1;
+	result = prime * result + ((id == null) ? 0 : id.hashCode());
 	result = prime * result + ((idMediafile == null) ? 0 : idMediafile.hashCode());
-	result = prime * result + ((pk == null) ? 0 : pk.hashCode());
 	return result;
     }
 
@@ -48,18 +58,20 @@ public class EpisodioModel {
 	if (getClass() != obj.getClass())
 	    return false;
 	EpisodioModel other = (EpisodioModel) obj;
+	if (id == null) {
+	    if (other.id != null)
+		return false;
+	} else if (!id.equals(other.id))
+	    return false;
 	if (idMediafile == null) {
 	    if (other.idMediafile != null)
 		return false;
 	} else if (!idMediafile.equals(other.idMediafile))
 	    return false;
-	if (pk == null) {
-	    if (other.pk != null)
-		return false;
-	} else if (!pk.equals(other.pk))
-	    return false;
 	return true;
     }
+
+    
 
     
     
